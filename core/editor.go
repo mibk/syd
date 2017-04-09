e62bc891faade11c50a68b10407ad9b5bdf8bcdc
Michal Bohuslávek <mbohuslavek@gmail.com>
2017-04-09 14:48:55 +0200

    Refactor Editor to package core

---
+++ core/editor.go
@@ -0,0 +1,80 @@
+package core
+
+import (
+	"os"
+
+	"github.com/mibk/syd/ui"
+	"github.com/mibk/syd/ui/term"
+	"github.com/mibk/syd/vi"
+	"golang.org/x/mobile/event/key"
+	"golang.org/x/mobile/event/mouse"
+)
+
+type Editor struct {
+	ui         *term.UI
+	events     chan ui.Event
+	vi         *vi.Parser
+	shouldQuit bool
+
+	wins []*Window
+	mode int
+}
+
+func NewEditor(u *term.UI) *Editor {
+	return &Editor{
+		ui:     u,
+		events: make(chan ui.Event),
+	}
+}
+
+func (ed *Editor) Main() {
+	for !ed.shouldQuit {
+		for _, win := range ed.wins {
+			win.LoadText()
+		}
+		ed.ui.Flush()
+		ev := <-ui.Events
+		if ev == ui.Quit {
+			return
+		}
+		switch ev := ev.(type) {
+		case key.Event:
+			ed.ui.Push_Key_Event(ev)
+		case mouse.Event:
+			// Temporary reasons...
+			ed.ui.Push_Mouse_Event(ev)
+		}
+	}
+}
+
+func (ed *Editor) NewWindow() {
+	ed.newWindow(BytesContent([]byte{}))
+}
+
+func (ed *Editor) NewWindowFile(filename string) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	mm, err := Mmap(f)
+	if err != nil {
+		return err
+	}
+	win := ed.newWindow(mm)
+	win.SetFilename(filename)
+	return nil
+}
+
+func (ed *Editor) newWindow(con Content) *Window {
+	win := NewWindow(ed.ui.NewWindow(), con)
+	ed.wins = append(ed.wins, win)
+	return win
+}
+
+func (ed *Editor) Close() error {
+	for _, win := range ed.wins {
+		// TODO: Check errors.
+		win.Close()
+	}
+	return nil
+}
--- syd.go
+++ syd.go
@@ -4,32 +4,21 @@ import (
 	"log"
 	"os"
 
-	"golang.org/x/mobile/event/key"
-	"golang.org/x/mobile/event/mouse"
-
 	"github.com/mibk/syd/core"
-	"github.com/mibk/syd/ui"
 	"github.com/mibk/syd/ui/term"
-	"github.com/mibk/syd/vi"
-)
-
-var (
-	UI       = &term.UI{}
-	filename = ""
 )
 
 func main() {
 	log.SetPrefix("syd: ")
 	log.SetFlags(0)
-	if err := UI.Init(); err != nil {
+
+	ui := &term.UI{}
+	if err := ui.Init(); err != nil {
 		log.Fatalln("initializing ui:", err)
 	}
-	defer UI.Close()
+	defer ui.Close()
 
-	ed := &Editor{
-		events: make(chan ui.Event),
-		vi:     vi.NewParser(),
-	}
+	ed := core.NewEditor(ui)
 	if len(os.Args) == 1 {
 		ed.NewWindow()
 	} else {
@@ -46,64 +35,3 @@ const (
 	ModeNormal = iota
 	ModeInsert
 )
-
-type Editor struct {
-	events     chan ui.Event
-	vi         *vi.Parser
-	shouldQuit bool
-
-	wins []*core.Window
-	mode int
-}
-
-func (ed *Editor) Main() {
-	for !ed.shouldQuit {
-		for _, win := range ed.wins {
-			win.LoadText()
-		}
-		UI.Flush()
-		ev := <-ui.Events
-		if ev == ui.Quit {
-			return
-		}
-		switch ev := ev.(type) {
-		case key.Event:
-			UI.Push_Key_Event(ev)
-		case mouse.Event:
-			// Temporary reasons...
-			UI.Push_Mouse_Event(ev)
-		}
-	}
-}
-
-func (ed *Editor) NewWindow() {
-	ed.newWindow(core.BytesContent([]byte{}))
-}
-
-func (ed *Editor) NewWindowFile(filename string) error {
-	f, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-	mm, err := core.Mmap(f)
-	if err != nil {
-		return err
-	}
-	win := ed.newWindow(mm)
-	win.SetFilename(filename)
-	return nil
-}
-
-func (ed *Editor) newWindow(con core.Content) *core.Window {
-	win := core.NewWindow(UI.NewWindow(), con)
-	ed.wins = append(ed.wins, win)
-	return win
-}
-
-func (ed *Editor) Close() error {
-	for _, win := range ed.wins {
-		// TODO: Check errors.
-		win.Close()
-	}
-	return nil
-}
